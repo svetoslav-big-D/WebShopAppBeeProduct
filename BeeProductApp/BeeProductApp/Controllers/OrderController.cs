feb10937d4156adfe83a36431c166999c2aa1447
svetoslav-big-D <s.lozanow2021@pmg-pernik.com>
2026-03-17 13:32:33 +0200

    part 5 page 9

---
+++ BeeProductApp/BeeProductApp/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using BeeProductApp.Core.Contracts;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeeProductApp.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IOrderService _orderService;
+
+        public OrderController(IProductService productService, IOrderService orderService)
+        {
+            _productService = productService;
+            _orderService = orderService;
+        }
+
+    }
+}
